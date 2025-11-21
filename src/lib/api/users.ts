c70b6da4f9194e6a78ce13fdfcbcf1739e5c2a73
Naman Bhayana <naman.bhayana13@gmail.com>
2025-11-21 08:50:54 +0530

    feat(api): add types and users fetchers

---
+++ src/lib/api/users.ts
@@ -0,0 +1,15 @@
+import axios from 'axios'
+import type { User } from '../types'
+
+const BASE_URL = 'https://jsonplaceholder.typicode.com'
+
+export const fetchUsers = async (): Promise<User[]> => {
+  const response = await axios.get<User[]>(`${BASE_URL}/users`)
+  return response.data
+}
+
+export const fetchUserById = async (id: number): Promise<User> => {
+  const response = await axios.get<User>(`${BASE_URL}/users/${id}`)
+  return response.data
+}
+
